d7b2e4b43c8d295e081dafa579cf70c93412c5c2
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2022-02-05 16:37:24 -0500

    dev TransactionAuthorizationViewModel is created

--- app/src/main/java/com/transactionapp/transactionauthorization/viewmodel/TransactionAuthorizationViewModel.kt
+++ app/src/main/java/com/transactionapp/transactionauthorization/viewmodel/TransactionAuthorizationViewModel.kt
@@ -1,4 +1,12 @@
 package com.transactionapp.transactionauthorization.viewmodel
 
+import com.transactionapp.base.domain.ResultData
+import com.transactionapp.base.framework.restapi.model.AuthorizationBody
+import com.transactionapp.transactionauthorization.domain.AuthorizationResponse
+
 interface TransactionAuthorizationViewModel {
+    fun onPostTransactionAuthorization(authorization: String, authorizationBody: AuthorizationBody)
+    fun handleTransactionAuthorizationResult(result: ResultData<AuthorizationResponse?>)
+    fun setErrorTransactionAuthorization(throwable: Throwable)
+    fun setTransactionAuthorizationData(value: AuthorizationResponse?)
 }
--- app/src/main/java/com/transactionapp/transactionauthorization/viewmodel/TransactionAuthorizationViewModelImpl.kt
+++ app/src/main/java/com/transactionapp/transactionauthorization/viewmodel/TransactionAuthorizationViewModelImpl.kt
@@ -1,8 +1,19 @@
 package com.transactionapp.transactionauthorization.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.transactionapp.base.domain.ResultData
+import com.transactionapp.base.framework.restapi.model.AuthorizationBody
+import com.transactionapp.transactionauthorization.domain.AuthorizationResponse
 import com.transactionapp.transactionauthorization.usecase.PostTransactionAuthorizationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.functions.Consumer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +21,44 @@ class TransactionAuthorizationViewModelImpl @Inject constructor(
     private val postTransactionAuthorizationUseCase: PostTransactionAuthorizationUseCase
 ): TransactionAuthorizationViewModel, ViewModel() {
 
+    private var postTransactionAuthorizationJob: Job? = null
 
+    private val transactionAuthorizationError: MutableLiveData<String> = MutableLiveData()
+    private val transactionAuthorizationResult: MutableLiveData<AuthorizationResponse> = MutableLiveData()
+
+    val transactionAuthorizationErrorLiveData:LiveData<String> = transactionAuthorizationError
+    val transactionAuthorizationResultLiveData:LiveData<AuthorizationResponse> = transactionAuthorizationResult
+
+    override fun onPostTransactionAuthorization(
+        authorization: String,
+        authorizationBody: AuthorizationBody
+    ) {
+        postTransactionAuthorizationJob?.cancel()
+        postTransactionAuthorizationJob = viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                postTransactionAuthorizationUseCase
+                    .postTransactionAuthorization(authorization, authorizationBody)
+                        .subscribe(Consumer {
+                                handleTransactionAuthorizationResult(it)
+                        })
+            }
+        }
+    }
+
+    override fun handleTransactionAuthorizationResult(result: ResultData<AuthorizationResponse?>) {
+        when(result){
+            is ResultData.Success -> setTransactionAuthorizationData(result.value)
+            is ResultData.Failure -> setErrorTransactionAuthorization(result.throwable)
+        }
+    }
+
+    override fun setErrorTransactionAuthorization(throwable: Throwable) {
+        transactionAuthorizationError.postValue(throwable.stackTraceToString())
+    }
+
+    override fun setTransactionAuthorizationData(value: AuthorizationResponse?) {
+        if (value != null){
+            transactionAuthorizationResult.postValue(value)
+        }
+    }
 }
